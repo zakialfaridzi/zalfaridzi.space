9aa9425d293507e6eb5d17cce4476e47618577b4
zakialfaridzi <zalfaridzi9@gmail.com>
2021-10-03 09:01:20 +0700

    font and icons opt

--- components/Navbar.js
+++ components/Navbar.js
@@ -5,6 +5,8 @@ import { useRouter } from "next/router";
 import userData from "@constants/data";
 import { Disclosure, Menu, Transition } from "@headlessui/react";
 import { MenuIcon, XIcon } from "@heroicons/react/outline";
+import { FiSun } from "@react-icons/all-files/fi/FiSun";
+import { FiMoon } from "@react-icons/all-files/fi/FiMoon";
 
 const navigation = [
   { name: "About", href: "/about", current: false },
@@ -167,35 +169,13 @@ export default function Navbar() {
               </div>
               <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
                 <button
-                  aria-label="Toggle Dark Mode"
-                  type="button"
-                  className="w-10 h-10 p-3 rounded focus:outline-none"
+                  className="p-2.5 border-[2px] dark:text-[#FAF089] dark:border-gray-700 focus:outline-none rounded-md border-thin hover:border-accent-200 dark:hover:border-accent-200 hover:text-accent-200 dark:hover:text-accent-200 focus:border-accent-200 dark:focus:border-accent-200 focus:text-accent-200 dark:focus:text-accent-200"
                   onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
                 >
-                  {mounted && (
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      viewBox="0 0 24 24"
-                      fill="currentColor"
-                      stroke="currentColor"
-                      className="w-4 h-4 text-yellow-500 dark:text-yellow-500"
-                    >
-                      {theme === "dark" ? (
-                        <path
-                          strokeLinecap="round"
-                          strokeLinejoin="round"
-                          strokeWidth={2}
-                          d="M12 3v1m0 16v1m9-9h-1M4 12H3m15.364 6.364l-.707-.707M6.343 6.343l-.707-.707m12.728 0l-.707.707M6.343 17.657l-.707.707M16 12a4 4 0 11-8 0 4 4 0 018 0z"
-                        />
-                      ) : (
-                        <path
-                          strokeLinecap="round"
-                          strokeLinejoin="round"
-                          strokeWidth={2}
-                          d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z"
-                        />
-                      )}
-                    </svg>
+                  {theme === "light" ? (
+                    <FiMoon size={20} />
+                  ) : (
+                    <FiSun size={20} />
                   )}
                 </button>
               </div>
